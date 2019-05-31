c1319f05bdedb95ea9d37e11789396db14c10a0e
Lloyd Bond <lloyd.bond@wework.com>
2019-05-31 17:16:55 -0400

    More tests passing

--- lib/wem2kmethods.ts
+++ lib/wem2kmethods.ts
@@ -4,5 +4,23 @@ export function encodeUUID(uuid: string, encodeFrom: BufferEncoding = 'utf8', en
 }
 
 export function transformUUIDToRaw(uuid: string): string {
-    return uuid;
+    const patterns = [
+        {
+            action: (u: string, _p: string): string => u,
+            pattern:'^[a-z0-9]{32}$',
+        },
+        {
+            action: (u: string, p: string): string => u.replace(p, '$1$2$3$4$5'),
+            pattern:'^([a-z0-9]{8})-([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{12})$',
+        },
+    ];
+    let rawuuid = '';
+    for (let i = 0; i < patterns.length && !rawuuid; i++) {
+        const pattern = new RegExp(patterns[i].pattern);
+        if (pattern.test(uuid)) {
+            rawuuid = patterns[i].action(uuid,patterns[i].pattern);
+        }
+    }
+
+    return rawuuid;
 }
