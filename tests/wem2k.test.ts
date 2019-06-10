fac7c1a52b57a2b0f4fe062146a93f36a15a18f0
Lloyd Bond <lloyd.bond@wework.com>
2019-06-10 16:53:44 -0400

    improve WeM2k tests descriptions

--- tests/wem2k.test.ts
+++ tests/wem2k.test.ts
@@ -2,28 +2,25 @@ import WeM2k from '../lib/wem2k';
 
 describe('WeM2k Tests', () => {
   let wem2ktest: WeM2k;
-  const simpleRawEncodedUUID = 'MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA=';
-  describe('Generic UUID tests for principleUUID', () => {
+  const uuid = '2b7a2019-13c5-4337-ba60-90b6437d3920';
+  const rawUUID = uuid.replace('-','');
+  describe('principleUUID', () => {
     beforeEach(() => {
       wem2ktest = new WeM2k('http://example.com',false);
     });
 
-    it('test generic uuid is encoded', () => {
-      const simpleUUID = '00000000-0000-0000-0000-000000000000';
-      const principleuuid = wem2ktest.principleUUID(simpleUUID);
-      expect(principleuuid).toStrictEqual(simpleRawEncodedUUID);
-    });
-
-    it('test generic raw uuid is encoded', () => {
-      const rawUUID = '00000000000000000000000000000000';
-      const principleuuid = wem2ktest.principleUUID(rawUUID);
-      expect(principleuuid).toStrictEqual(simpleRawEncodedUUID);
-    });
-
-    it('test complex uuid is encoded', () => {
-      const complexUUID = '2b7a2019-13c5-4337-ba60-90b6437d3920';
-      const principleuuid = wem2ktest.principleUUID(complexUUID);
+    it('encodes uuid', () => {
+      const principleuuid = wem2ktest.principleUUID(uuid);
       expect(principleuuid).toStrictEqual('MmI3YTIwMTkxM2M1NDMzN2JhNjA5MGI2NDM3ZDM5MjA=');
+    });
+
+    it('encodes raw uuid', () => {
+      const principleuuid = wem2ktest.principleUUID(rawUUID);
+      expect(principleuuid).toStrictEqual('MmI3YTIwMTkxM2M1NDMzN2JhNjA5MGI2NDM3ZDM5MjA=');
+    });
+
+    it('encoded uuid and encoded raw uuid are equal', () => {
+      expect(wem2ktest.principleUUID(uuid)).toStrictEqual(wem2ktest.principleUUID(rawUUID));
     });
   });
 });
