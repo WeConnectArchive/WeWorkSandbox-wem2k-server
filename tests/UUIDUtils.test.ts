f847cd0c584bc0975cb1864ce53db7a7e8aa971d
Lloyd Bond <lloyd.bond@wework.com>
2019-06-13 10:20:51 -0400

    pulled out length test and reduced the extra method

--- tests/UUIDUtils.test.ts
+++ tests/UUIDUtils.test.ts
@@ -1,10 +1,5 @@
 import * as UUID from '../lib/UUIDUtils';
 
-function validateParameterMatch(actual: string, expected: string) {
-    expect(actual).toHaveLength(expected.length);
-    expect(actual).toEqual(expected);
-}
-
 describe('Test UUIDUtils Methods', () => {
     const prettyUUID = '2b7a2019-13c5-4337-ba60-90b6437d3920';
     const rawTestUUID = '2b7a201913c54337ba6090b6437d3920';
@@ -12,27 +7,27 @@ describe('Test UUIDUtils Methods', () => {
     describe('sanitizeUUID', () => {
         it('converts a pretty uuid to a rawUUID', () => {
             const actualRawUUID = UUID.sanitizeUUID(prettyUUID);
-            validateParameterMatch(actualRawUUID,rawTestUUID);
+            expect(actualRawUUID).toEqual(rawTestUUID);
         });
         it('converts a raw uuid to a rawUUID', () => {
             const actualRawUUID = UUID.sanitizeUUID(rawTestUUID);
-            validateParameterMatch(actualRawUUID,rawTestUUID);
+            expect(actualRawUUID).toEqual(rawTestUUID);
         });
         it('trims whitespace', () => {
-            const actualRawUUID = UUID.sanitizeUUID('\t' + prettyUUID + '  ');
-            validateParameterMatch(actualRawUUID,rawTestUUID);
+            expect(UUID.sanitizeUUID('\t' + prettyUUID + '\t')).toEqual(rawTestUUID);
+            expect(UUID.sanitizeUUID('  ' + prettyUUID + ' ')).toEqual(rawTestUUID);
         });
         it('matches UUID case insensitive', () => {
             const actualRawUUID = UUID.sanitizeUUID('2B7A2019-13c5-4337-ba60-90b6437d3920');
-            validateParameterMatch(actualRawUUID,'2B7A201913c54337ba6090b6437d3920');
+            expect(actualRawUUID).toEqual('2B7A201913c54337ba6090b6437d3920');
         });
 
         it('returns an empty string when the input is not a UUID', () => {
             const emptyString = '';
-            validateParameterMatch(UUID.sanitizeUUID(''),emptyString);
-            validateParameterMatch(UUID.sanitizeUUID(' áñúó´p;-!@#$-%^&*-()+=-0987654321qw'),emptyString);
-            validateParameterMatch(UUID.sanitizeUUID('00000000-0000-0000-0000-0000000000001'),emptyString);
-            validateParameterMatch(UUID.sanitizeUUID('00000000-0000-0000-0000-00000000000'),emptyString);
+            expect(UUID.sanitizeUUID('')).toEqual(emptyString);
+            expect(UUID.sanitizeUUID(' áñúó´p;-!@#$-%^&*-()+=-0987654321qw')).toEqual(emptyString);
+            expect(UUID.sanitizeUUID('00000000-0000-0000-0000-0000000000001')).toEqual(emptyString);
+            expect(UUID.sanitizeUUID('00000000-0000-0000-0000-00000000000')).toEqual(emptyString);
         });
     });
 
@@ -40,7 +35,7 @@ describe('Test UUIDUtils Methods', () => {
         it('encodes a raw uuid to base64 encoding', () => {
             const actualEncodedUUID = UUID.encodeUUID(rawTestUUID);
             const expectedEncodedValue = 'K3ogGRPFQze6YJC2Q305IA==';
-            validateParameterMatch(actualEncodedUUID, expectedEncodedValue);
+            expect(actualEncodedUUID).toEqual(expectedEncodedValue);
         });
 
 
