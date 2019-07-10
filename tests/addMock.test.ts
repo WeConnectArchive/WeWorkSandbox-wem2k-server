e15444d66447c66ce98b966370afd47ed21fe91a
Mehera Pokale <mehera.pokale@wework.com>
2019-07-10 12:51:09 -0400

    minor changes to test

--- tests/addMock.test.ts
+++ tests/addMock.test.ts
@@ -1,18 +1,26 @@
+import nock from 'nock';
 import WeM2k from '../lib/wem2k';
 
 describe('Add mock', () => {
-  test('it adds mock to the existing scope', () => {
+  test('it adds a mock to the existing mocks', () => {
     const wem2ktest = new WeM2k('http://example.com', false);
+
+    nock('http://www.example.com')
+      .get('/resource')
+      .reply(200, 'domain matched');
+    expect(nock.pendingMocks().length).toEqual(1);
+
     const mockDef = {
         method: 'get',
         path: '/api',
         response: 'response from newly added mock',
         status: 200,
     };
-    const scope = wem2ktest.addMock(mockDef);
-    expect(scope.length).toEqual(1);
+    wem2ktest.addMock(mockDef);
+
+    expect(nock.pendingMocks().length).toEqual(2);
   });
-  test('it sets scope to responseGenerator', () => {
+  test('it sets nock uri to responseGenerator of server', () => {
     const responseGenerator = 'http://example.com';
     const wem2ktest = new WeM2k(responseGenerator, false);
     const mockDef = {
